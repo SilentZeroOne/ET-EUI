1fd4cef880ded5f56137c9e9d8ba6f9597d6d717
Tivon Zhu <a792038524@gmail.com>
2022-08-08 15:09:03 +0800

    移动utilities到HotfixMono

---
+++ Unity/Assets/HotfixMono/Utilities/DataCollection.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace ET
+{
+    [System.Serializable]
+    public class ItemDetails
+    {
+        public int ItemId;
+
+        public string Name;
+
+        public Sprite ItemIcon;
+
+        public Sprite ItemOnWorldSprite;
+
+        public string ItemDescription;
+
+        public int itemUseRadius;
+
+        public bool CanPickedUp;
+
+        public bool CanDropped;
+
+        public bool CanCarried;
+
+        public int ItemPrice;
+
+        [Range(0,1)]
+        public float SellPercentage;
+    }
+}
